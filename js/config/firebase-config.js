8fd30b80684a1c673765370c611ad143a3f5c9f3
Michael Hills <michael@55Rush.com>
2026-01-08 10:26:25 -0500

    Add Firebase config credentials

--- js/config/firebase-config.js
+++ js/config/firebase-config.js
@@ -13,12 +13,13 @@
  */
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC_XpIE98F0uisHXBZS71nkWppTHp9on8c",
+  authDomain: "spintowin-20c4e.firebaseapp.com",
+  projectId: "spintowin-20c4e",
+  storageBucket: "spintowin-20c4e.firebasestorage.app",
+  messagingSenderId: "625209568446",
+  appId: "1:625209568446:web:ddbd663683701c130042ef",
+  measurementId: "G-PKXELYZ5Z6"
 };
 
 // Check if Firebase is loaded
